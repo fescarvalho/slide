4cb02bf661729ec7ac64350c0bbca78a7e9022a1
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2022-03-22 12:44:39 -0300

    controles de navegacao final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import { SlideNav } from "./slide.js";
+import SlideNav from "./slide.js";
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 slide.addArrow(".prev", ".next");
-slide.addControlEvent();
+slide.addControlEvent(".custom-controls");
